44e0f94a42cd5b3b923d96dbd5264ab93587e5d1
Nguyen Cong Minh <minhnc@deha-soft.com>
2023-05-25 22:36:23 +0700

    add element

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+
+const listQuote = [];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="quote-box">
+        <div id="text">
+          Our lives begin to end the day we become silent about things that
+          matter.
+        </div>
+        <div id="author">- Martin Luther King Jr.</div>
+        <div>
+          <a
+            href="https://twitter.com/intent/tweet"
+            rel="noreferrer"
+            target="_blank"
+            id="tweet-quote"
+          >
+            Tag
+          </a>
+          <button id="new-quote">New quote</button>
+        </div>
+      </div>
     </div>
   );
 }
